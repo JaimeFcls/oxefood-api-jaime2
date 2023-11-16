9b840ea0d1f0535c8a14891ccb1990013b9c56ec
JaimeFcls <119618310+JaimeFcls@users.noreply.github.com>
2023-11-16 15:11:55 -0300

    tome

---
+++ src/main/java/br/com/ifpe/oxefoodapiJaime/api/Categoria/CategoriaProdutoRequest.java
@@ -0,0 +1,23 @@
+package br.com.ifpe.oxefoodapiJaime.api.Categoria;
+
+import br.com.ifpe.oxefoodapiJaime.modelo.categoriaproduto.CategoriaProduto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaProdutoRequest {
+    private String descricao;
+
+    public CategoriaProduto build() {
+
+        return CategoriaProduto.builder()
+                .descricao(descricao)
+                .build();
+    }
+
+}
